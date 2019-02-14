9ec08679cc4dee156e38037a3113d6f7b57d1a43
djl57 <18367317878@163.com>
2019-02-14 16:20:33 +0800

    加了19个svg

--- src/views/svgIcons/requireIcons.js
+++ src/views/svgIcons/requireIcons.js
@@ -1,6 +1,7 @@
 const req = require.context('../../icons/svg', false, /\.svg$/)
-const requireAll = requireContext => requireContext.keys()
-
+const requireAll = requireContext => {
+  return requireContext.keys()
+}
 const re = /\.\/(.*)\.svg/
 const icons = requireAll(req).map(i => {
   return i.match(re)[1]
